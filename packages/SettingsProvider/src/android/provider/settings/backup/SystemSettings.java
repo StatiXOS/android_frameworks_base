3e3475c65a32a53d7f46703c2270a2ea2b2fb46d
Steve Kondik <steve@cyngn.com>
2016-05-02 04:01:43 -0700

    Keyguard: Allow disabling fingerprint wake-and-unlock
    arrow-11.0:
     - Ditch the overlay and make it runtime configurable
    
     * When the fingerprint sensor is embedded in the power key,
       wake-and-unlock is total chaos. Add an option to disable it.
     * The default behavior is unchanged.
    
    Change-Id: I50c0a857daba92c17470d8089aca94099c792956
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -87,6 +87,7 @@ public class SystemSettings {
         Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
         Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN,
         Settings.System.FOOTER_TEXT_SHOW,
-        Settings.System.QS_HEADER_BG_ALPHA
+        Settings.System.QS_HEADER_BG_ALPHA,
+        Settings.System.FP_WAKE_UNLOCK
     };
 }
