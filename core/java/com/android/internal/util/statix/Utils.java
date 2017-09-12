90ed226e9b8e86003ae0163e542605d9d2bb0dc0
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Utils: Add api to take screenshots
    abc: extracted from DUI
    Change-Id: I5aa5635e1e51a50bda4f766395359b62647a642d

--- core/java/com/android/internal/util/statix/Utils.java
+++ core/java/com/android/internal/util/statix/Utils.java
@@ -35,15 +35,20 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.view.IWindowManager;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
+import android.view.WindowManagerGlobal;
 
 import com.android.internal.statusbar.IStatusBarService;
 
 import java.util.Locale;
 
 public class Utils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     // Check to see if device is WiFi only
     public static boolean isWifiOnly(Context context) {
@@ -159,6 +164,15 @@ public class Utils {
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
             }
         }, 20);
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void toggleCameraFlash() {
