53a289be5df1b5297147dd1851a5586fe0511255
maxwen <max.weninger@gmail.com>
2017-12-12 13:29:35 +0100

    SystemUI: Add VPN to the icon blacklist in tuner
    Change-Id: I579d1ec7a6d66ac7055cce7d7e905bae7dc5cd9a
    Signed-off-by: Adithya <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -60,6 +60,7 @@ public class StatusBarSignalPolicy
     private boolean mHideAirplane;
     private boolean mHideMobile;
     private boolean mHideEthernet;
+    private boolean mHideVpn;
 
 
     @Inject
@@ -98,7 +99,7 @@ public class StatusBarSignalPolicy
     }
 
     private void updateVpn() {
-        boolean vpnVisible = mSecurityController.isVpnEnabled();
+        boolean vpnVisible = mSecurityController.isVpnEnabled() && !mHideVpn;
         int vpnIconId = currentVpnIconId(
                 mSecurityController.isVpnBranded(),
                 mSecurityController.isVpnValidated());
@@ -137,12 +138,14 @@ public class StatusBarSignalPolicy
         boolean hideAirplane = hideList.contains(mSlotAirplane);
         boolean hideMobile = hideList.contains(mSlotMobile);
         boolean hideEthernet = hideList.contains(mSlotEthernet);
+        boolean hideVpn = hideList.contains(mSlotVpn);
 
         if (hideAirplane != mHideAirplane || hideMobile != mHideMobile
-                || hideEthernet != mHideEthernet) {
+                || hideEthernet != mHideEthernet || hideVpn != mHideVpn) {
             mHideAirplane = hideAirplane;
             mHideMobile = hideMobile;
             mHideEthernet = hideEthernet;
+            mHideVpn = hideVpn;
         }
     }
 
