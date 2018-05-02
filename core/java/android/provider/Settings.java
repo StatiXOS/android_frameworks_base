6b4b5da016bea0dbfc818fd25e9d96cb3546ea85
rascarlo <rascarlo@gmail.com>
2018-05-02 15:49:25 +0200

    Volume Rocker Wake [1/2]
    Change-Id: If5af441eda6321565b905c266b41881d4f53cb37

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4461,6 +4461,12 @@ public final class Settings {
        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
