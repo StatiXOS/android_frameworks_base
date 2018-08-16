7d7eb1aa7a2d2ad1c0d3b505c49a47a334e18b8c
Surge Raval <Surge1223@gmail.com>
2018-08-16 10:41:49 +0000

    base: modify/define custom overlay management
    Change-Id: I37376416fdd847e354075d31ee3ecd1f83df0cc7
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- core/jni/fd_utils.cpp
+++ core/jni/fd_utils.cpp
@@ -87,6 +87,7 @@ bool FileDescriptorWhitelist::IsAllowed(const std::string& path) const {
   static const char* kOverlaySubdir = "/system/vendor/overlay-subdir/";
   static const char* kSystemProductOverlayDir = "/system/product/overlay/";
   static const char* kProductOverlayDir = "/product/overlay";
+  static const char* kThemeOverlayDir = "/data/system/theme";
   static const char* kApkSuffix = ".apk";
 
   if ((android::base::StartsWith(path, kOverlayDir)
@@ -95,6 +96,7 @@ bool FileDescriptorWhitelist::IsAllowed(const std::string& path) const {
        || android::base::StartsWith(path, kSystemProductOverlayDir)
        || android::base::StartsWith(path, kProductOverlayDir)
        || android::base::StartsWith(path, "/system/overlay"))
+       || android::base::StartsWith(path, kThemeOverlayDir))
       && android::base::EndsWith(path, kApkSuffix)
       && path.find("/../") == std::string::npos) {
     return true;
