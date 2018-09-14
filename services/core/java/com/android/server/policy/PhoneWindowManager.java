568c1d01a7d025a437a6e5c4cd8ccb95253b27c4
vm03 <vasy@vasy.ru>
2018-09-14 04:04:50 -0500

    base: Fix proximity check on non power key
    To test, enable "Settings >> Display >> Prevent accidental wake-up"
    and "Settings >> Buttons >> Home button Wake up device", hold your
    hand over the proximity sensor and turn your phone on by home key.
    
    Prior to this change, it will wake up the phone and after the change
    it will not.
    
    Change-Id: Id4d969f879d97a231144fda8aca84f2c7d95156e

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6165,7 +6165,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (isValidGlobalKey(keyCode)
                 && mGlobalKeyManager.shouldHandleGlobalKey(keyCode, event)) {
             if (isWakeKey) {
-                wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY");
+                wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey,
+                       "android.policy:KEY", true);
             }
             return result;
         }
@@ -6486,7 +6487,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         if (isWakeKey) {
-            wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY");
+            wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY", true);
         }
 
         return result;
