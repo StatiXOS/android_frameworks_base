41bcfec566b0c3f7f65c792760c7181f4c10a8d7
xyyx <xyyx@mail.ru>
2018-10-01 21:31:43 +0800

    Buttons: Disable haptic in Ambient or Always on
    Change-Id: I4a909f445ce75fbd5e216af6a75d31ab4424d5ed

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4000,7 +4000,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && event.getRepeatCount() == 0;
 
         if (!virtualKey) {
-            if (isHwKeysDisabled() || keyguardOn()) {
+            if (isHwKeysDisabled() || keyguardOn() || isDozeMode()) {
                 useHapticFeedback = false;
             }
         }
