64ea2a9cf8b29ee9b34022ac74440ba33efa3108
ezio84 <brabus84@gmail.com>
2018-10-28 16:32:28 +0100

    Add and observe CustomDoze settings.
    **IMPORTANT: Custom doze brightness options work correctly
    with doze_brightness_sensor_type and config_doze_brightness_sensor_to_brightness,
    so if "config_allowAutoBrightnessWhileDozing" is set to false.
    TL;DR explanation:
    
    The overlay "config_allowAutoBrightnessWhileDozing" allows DisplayPowerController to
    use automatic brightness sensor to adjust doze brighthness like in normal
    screen mode.
    But some devices like Pixel have in the device tree an overlay for
    doze_brightness_sensor_type (which sets the builtin low-power brightness
    sensor of the device if available - e.g. com.google.sensor.binned_brightness
    for Taimen) and the proper array to convert values coming from that sensor
    to standard brightness values:
        <integer-array name="config_doze_brightness_sensor_to_brightness">
            <item>-1</item> <!-- 0: OFF -->
            <item>2</item> <!-- 1: NIGHT -->
            <item>7</item> <!-- 2: LOW -->
            <item>27</item> <!-- 3: HIGH -->
            <item>28</item> <!-- 4: SUN -->
        </integer-array>
    This specific sensor is managed straight by SystemUI DozeFactory to
    create a DozeScreenBrightness that automatically changes brightness
    during the ambient session (it's not as granular as the full auto
    brightness, but it's probably supposed to consume less power).
    
    Change-Id: I05adb0f88b0aaca39d954dcec7c33c41c579b90b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4923,7 +4923,32 @@ public final class Settings {
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
          * the setting value. See an example above.
+	 */
+
+	/**
+         * Custom Ambient tilt gesture
+         * @hide
          */
+        public static final String CUSTOM_AMBIENT_TILT_GESTURE = "custom_ambient_tilt_gesture";
+
+        /**
+         * Custom Ambient handwave gesture
+         * @hide
+         */
+        public static final String CUSTOM_AMBIENT_HANDWAVE_GESTURE = "custom_ambient_handwave_gesture";
+
+         /**
+         * Custom Ambient pocketmode gesture
+         * @hide
+         */
+        public static final String CUSTOM_AMBIENT_POCKETMODE_GESTURE = "custom_ambient_pocketmode_gesture";
+
+        /**
+         * Ambient screen settings
+         * "screen_brightness_array=0:1:2:3:4"
+         * @hide
+         */
+        public static final String ALWAYS_ON_DISPLAY_CONSTANTS_CUST = "always_on_display_constants_cust";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
