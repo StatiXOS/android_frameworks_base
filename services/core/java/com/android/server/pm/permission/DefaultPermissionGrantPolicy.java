65f8be0a1811200acc5586a30a283887694008f5
shagbag913 <sh4gbag913@gmail.com>
2019-01-20 06:34:08 -0600

    DefaultPermissionGrantPolicy: grant Google sounds picker storage perms
    Change-Id: I2926cdeb52bab2648a0947663e428338b113f312

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -978,6 +978,12 @@ public final class DefaultPermissionGrantPolicy {
             grantRuntimePermissions(weatherClientPackage, LOCATION_PERMISSIONS, userId);
         }
 
+        // Google sound picker
+        PackageParser.Package googleSoundPackage = getSystemPackage("com.google.android.soundpicker");
+        if (googleSoundPackage != null) {
+            grantRuntimePermissions(googleSoundPackage, STORAGE_PERMISSIONS, true, userId);
+        }
+
         if (mPermissionGrantedCallback != null) {
             mPermissionGrantedCallback.onDefaultRuntimePermissionsGranted(userId);
         }
