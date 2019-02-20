a361bab3483fb81a76d7797f6a5ebcf9c04561e9
bigrushdog <randall.rushing@gmail.com>
2019-02-20 15:19:31 -0800

    ActiveEdge: Implement SmartActions for action handling [1/3]
    Change-Id: I5cf08aeb3930ad0c143ac9f7dac036ec3003def3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8414,7 +8414,7 @@ public final class Settings {
          *
          * @hide
          */
-        public static final String SQUEEZE_SELECTION = "squeeze_selection";
+        public static final String SQUEEZE_SELECTION_SMART_ACTIONS = "squeeze_selection_smart_actions";
 
         /**
          * Display style of the status bar battery information
