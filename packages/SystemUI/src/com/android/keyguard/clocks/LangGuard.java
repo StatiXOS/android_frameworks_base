45705191d84c03eaad55fc528d27ffd333842423
ovenoboyo <ovenoboyo@gmail.com>
2019-04-18 13:46:38 +0000

    Q Clock - Add PT and FR
    Change-Id: Ie415121f672f8efccb42d7dfad1484b6a4e8a391

--- packages/SystemUI/src/com/android/keyguard/clocks/LangGuard.java
+++ packages/SystemUI/src/com/android/keyguard/clocks/LangGuard.java
@@ -31,6 +31,17 @@ public class LangGuard {
     public static String evaluateExMin (String lang, int units, String[] TensString, String[] UnitsString, int tens) {
         String numString = "";
         switch (lang) {
+
+            case "pt":
+                numString = TensString[tens] + "e " + UnitsString[units].toLowerCase();
+                return numString;
+
+            case "fr":
+                if (units == 1) {
+                    numString = TensString[tens] + "et un";
+                    return numString;
+                }
+
             case "it":
                 if (units == 1) {
                     numString = TensString[tens].substring(0, TensString[tens].length() - 1)+
@@ -50,6 +61,17 @@ public class LangGuard {
     public static String evaluateExHr (String lang, int units, String[] TensString, String[] UnitsString, int tens) {
         String numString = "";
         switch (lang) {
+
+            case "pt":
+                numString = TensString[tens] + "e " + UnitsString[units].toLowerCase();
+                return numString;
+
+            case "fr":
+                if (units == 1) {
+                    numString = TensString[tens] + "et un";
+                    return numString;
+                }
+
             case "it":
                 if (units == 1) {
                     numString = TensString[tens].substring(0, TensString[tens].length() - 1)+
