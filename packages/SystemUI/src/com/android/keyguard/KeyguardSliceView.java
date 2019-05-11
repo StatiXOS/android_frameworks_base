e79cb4c2d1ee3e43b1ca9f5a31456d49a3c4b76a
ElDainosor <eldainosor@gmail.com>
2019-05-11 01:39:53 +0000

    base: add BTLG back [1/2]
    Change-Id: I43505c25a4d0c0d1f8c8d16d20e488515f60aa9b

--- packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
@@ -316,8 +316,24 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
         updateTextColors();
     }
 
+    public void setViewsTextStyles(float textSp, boolean textAllCaps) {
+        int childCount = mRow.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View v = mRow.getChildAt(i);
+            if (v instanceof Button) {
+                ((Button) v).setLetterSpacing(textSp);
+                ((Button) v).setAllCaps(textAllCaps);
+            }
+        }
+    }
+
     public void setViewBackground(Drawable drawRes) {
+        setViewBackground(drawRes, 255);
+    }
+
+    public void setViewBackground(Drawable drawRes, int bgAlpha) {
         mRow.setBackground(drawRes);
+        mRow.getBackground().setAlpha(bgAlpha);
     }
 
     public void setViewBackgroundResource(int drawRes) {
@@ -518,8 +534,8 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
             int childCount = getChildCount();
             for (int i = 0; i < childCount; i++) {
                 View child = getChildAt(i);
-                if (child instanceof KeyguardSliceTextView) {
-                    ((KeyguardSliceTextView) child).setMaxWidth(width / 3);
+                if (child instanceof KeyguardSliceButton && childCount > 3) {
+                    ((KeyguardSliceButton) child).setMaxWidth(width / childCount);
                 }
             }
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
