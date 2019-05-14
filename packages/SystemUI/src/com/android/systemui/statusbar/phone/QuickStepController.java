f683941ffc2d39e0d5cce2d194d4882bd914ec9a
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2019-05-13 20:22:37 -0400

    Full gesture navbar: add haptic feedback to back action
    Change-Id: I6ee011255929dab3053403e63aa36f698fe1a35d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
@@ -44,6 +44,7 @@ import android.os.RemoteException;
 import android.util.FloatProperty;
 import android.util.Log;
 import android.util.Slog;
+import android.view.HapticFeedbackConstants;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
@@ -366,6 +367,7 @@ public class QuickStepController implements GestureHelper {
                 if (mBackActionScheduled) {
                     mBackActionScheduled = false;
                     ActionHandler.performTask(mContext, ActionHandler.SYSTEMUI_TASK_BACK);
+  		    mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                 } else {
                     endQuickScrub(true /* animate */);
                 }
