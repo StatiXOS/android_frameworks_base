8dae1298fd6d0c66399992012ab12e28ba435b5c
Stefen <xstefen@gmail.com>
2019-05-23 23:08:01 -0500

    fod
    Change-Id: Ic3424e9a582a363f70f10460a58285ec627cf1d5
    Signed-off-by: Stefen <xstefen@gmail.com>

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -54,7 +54,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-slices-builders \
     android-arch-core-runtime \
     android-arch-lifecycle-extensions \
-    statix-support-lottie
+    statix-support-lottie \
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     SystemUI-tags \
