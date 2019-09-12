684ecbaffb023edf4db12b456b5abcb33968c9b7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-12 15:38:15 -0300

    SystemUI: Add method for getting estimated battery time
    Turbo implementation isn't working, so use aosp impl for now
    
    Change-Id: I8eda81c0cf7035b88e1a2adcdd5fdedb8d35c038
    (cherry picked from commit 9bd8cda9b9a94fae718690e2a9e61a62ef7d6b3d)

--- packages/SystemUI/src/com/android/systemui/power/EnhancedEstimatesImpl.java
+++ packages/SystemUI/src/com/android/systemui/power/EnhancedEstimatesImpl.java
@@ -1,7 +1,19 @@
 package com.android.systemui.power;
 
+import android.content.Context;
+import android.os.BatteryStats;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.os.UserManager;
+
+import com.android.internal.os.BatteryStatsHelper;
+
 import com.android.settingslib.fuelgauge.Estimate;
 import com.android.settingslib.fuelgauge.EstimateKt;
+<<<<<<< HEAD   (b3d461 utils: Introduce FileUtils class)
+=======
+import com.android.settingslib.utils.PowerUtil;
+>>>>>>> CHANGE (9bd8cd SystemUI: Add method for getting estimated battery time)
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -9,21 +21,46 @@ import javax.inject.Singleton;
 @Singleton
 public class EnhancedEstimatesImpl implements EnhancedEstimates {
 
+    BatteryStatsHelper mBatteryStatsHelper;
+    UserManager mUserManager;
+
     @Inject
-    public EnhancedEstimatesImpl() {
+    public EnhancedEstimatesImpl(Context context) {
+        mBatteryStatsHelper = new BatteryStatsHelper(context,
+                true /* collectBatteryBroadcast */);
+        mUserManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
     }
 
     @Override
     public boolean isHybridNotificationEnabled() {
-        return false;
+        return true;
     }
 
     @Override
     public Estimate getEstimate() {
+<<<<<<< HEAD   (b3d461 utils: Introduce FileUtils class)
         // Returns an unknown estimate.
         return new Estimate(EstimateKt.ESTIMATE_MILLIS_UNKNOWN,
                 false /* isBasedOnUsage */,
                 EstimateKt.AVERAGE_TIME_TO_DISCHARGE_UNKNOWN);
+=======
+        try{
+            mBatteryStatsHelper.create((Bundle) null);
+            mBatteryStatsHelper.clearStats();
+            mBatteryStatsHelper.refreshStats(BatteryStats.STATS_SINCE_CHARGED, mUserManager.getUserProfiles());
+            BatteryStats stats = mBatteryStatsHelper.getStats();
+            if (stats != null){
+                long remaining = stats.computeBatteryTimeRemaining(PowerUtil.convertMsToUs(
+                        SystemClock.elapsedRealtime()));
+                if (remaining != -1){
+                    return new Estimate(PowerUtil.convertUsToMs(remaining), false,
+                            EstimateKt.AVERAGE_TIME_TO_DISCHARGE_UNKNOWN);
+                }
+            }
+        } catch (Exception e) {
+        }
+        return null;
+>>>>>>> CHANGE (9bd8cd SystemUI: Add method for getting estimated battery time)
     }
 
     @Override
