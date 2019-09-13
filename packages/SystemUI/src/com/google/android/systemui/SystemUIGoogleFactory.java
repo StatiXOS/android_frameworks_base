b773898bd6b4b9281a20bc606637fb8166ad3872
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-12 23:06:07 -0300

    SystemUI: Implement Smart space
    Includes weather on lockscreen and cool stuff on ambient display
    
    Code reverse engineered from crosshatch
    
    Requires Google app installed
    
    Change-Id: Ice5ea4fd43412cf916daefa6521fc8b55f14f199

--- packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
+++ packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
@@ -1,6 +1,13 @@
 package com.google.android.systemui;
 
 import com.android.systemui.SystemUIFactory;
+import com.android.systemui.statusbar.NotificationLockscreenUserManager;
 
 public class SystemUIGoogleFactory extends SystemUIFactory {
+
+    @Override
+    public NotificationLockscreenUserManager provideNotificationLockscreenUserManager(Context context) {
+        return new NotificationLockscreenUserManagerGoogle(context);
+    }
+
 }
