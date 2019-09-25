819b340693a264dead72356fe20f73be906d191e
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    [1/2] base: Gesture Navigation: Configure the height of back gesture
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4532,6 +4532,13 @@ public final class Settings {
          */
         public static final String GLOBAL_ACTIONS_SETTINGS = "global_actions_settings";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
+        /** @hide */
+        private static final Validator BACK_GESTURE_HEIGHT_VALIDATOR =
+                ANY_INTEGER_VALIDATOR;
+
         /**
          * Whether to display lock in the power menu
          *
@@ -4632,6 +4639,7 @@ public final class Settings {
             ALARM_ALERT,
             NOTIFICATION_LIGHT_PULSE,
             SCREENSHOT_SHUTTER_SOUND,
+            BACK_GESTURE_HEIGHT,
         };
 
         /**
@@ -4752,6 +4760,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(SCREENSHOT_SHUTTER_SOUND);
+            PRIVATE_SETTINGS.add(BACK_GESTURE_HEIGHT);
         }
 
         /**
@@ -4847,6 +4856,7 @@ public final class Settings {
             VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
             VALIDATORS.put(SCREENSHOT_SHUTTER_SOUND,
                     SCREENSHOT_SHUTTER_SOUND_VALIDATOR);
+            VALIDATORS.put(BACK_GESTURE_HEIGHT, BACK_GESTURE_HEIGHT_VALIDATOR);
         }
 
         /**
