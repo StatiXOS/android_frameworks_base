32df607104b86dac10f3439bf4a5cbf6cf880505
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2019-09-28 14:52:55 +0200

    SystemUI: Show something usefull in the QS footer
     * Show StatiXOS <base version> instead of instead of that useless build version.
    
    Change-Id: I012b70d5b1ad247f7ee34b545c043a6d4d556475

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -31,6 +31,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
@@ -164,12 +165,10 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
 
     private void setBuildText() {
         TextView v = findViewById(R.id.build);
+        String baseVersion = SystemProperties.get("ro.statix.base.version");
         if (v == null) return;
         if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
-            v.setText(mContext.getString(
-                    com.android.internal.R.string.bugreport_status,
-                    Build.VERSION.RELEASE,
-                    Build.ID));
+            v.setText("StatiXOS" + baseVersion);
             v.setVisibility(View.VISIBLE);
         } else {
             v.setVisibility(View.GONE);
