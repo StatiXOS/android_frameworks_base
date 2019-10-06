63bd4134c3aef8f202df1dadd23814cb28ea4785
maxwen <max.weninger@gmail.com>
2019-10-06 15:33:30 -0400

    Use statix fingerprint for ShortcutService
    make sure cached data gets updated
    no more derped deep shortcuts text and images
    
    Change-Id: I65d743a4991f92739e59b4655dcc086bd36a5401

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -4302,7 +4302,7 @@ public class ShortcutService extends IShortcutService.Stub {
 
     // Injection point.
     String injectBuildFingerprint() {
-        return Build.FINGERPRINT;
+        return Build.STATIX_FINGERPRINT;
     }
 
     final void wtf(String message) {
