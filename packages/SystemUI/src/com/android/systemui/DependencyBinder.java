cb04752f0f66322c580ba2770637174486406474
maxwen <max.weninger@gmail.com>
2019-11-29 23:41:24 +0100

    SystemUI: add CustomSettingsService with dagger injection
    easy way to add observer to omni settings - in Settings.System ONLY
    like TunerService
    
    no more need to create a dedicated ContentObserver simply use
    
     Dependency.get(CustomSettingsService.class).addIntObserver(this, Settings.System.USE_OLD_MOBILETYPE);
    
    and add CustomSettingsObserver interface
    as POC change the old style mobile data icon to use it
    
    Change-Id: I2ad9c8dc70913319e405ae71bae30049c6c2a649

--- packages/SystemUI/src/com/android/systemui/DependencyBinder.java
+++ packages/SystemUI/src/com/android/systemui/DependencyBinder.java
@@ -16,6 +16,8 @@
 
 package com.android.systemui;
 
+import com.android.systemui.custom.CustomSettingsService;
+import com.android.systemui.custom.CustomSettingsServiceImpl;
 import com.android.systemui.appops.AppOpsController;
 import com.android.systemui.appops.AppOpsControllerImpl;
 import com.android.systemui.classifier.FalsingManagerProxy;
@@ -247,6 +249,9 @@ public abstract class DependencyBinder {
     /**
      */
     @Binds
+
     public abstract RecordingController provideRecordingController(
             RecordingControllerImpl controllerImpl);
+
+    public abstract CustomSettingsService provideCustomSettingsService(CustomSettingsServiceImpl controllerImpl);
 }
