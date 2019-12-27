a71f255ea2e568d6bf68421eae7c57bb7f8297eb
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-12-27 12:34:35 +0100

    base: overlays: Hide pill in full gestural nav mode [1/2]
    - Originally extracted and decompiled from the HideNavBar magisk moudle.
    
    - Adjusted for CMake compilation and package names are changed as per
      our needs to dynamically load the overlays.
    
    source: https://github.com/Magisk-Modules-Repo/HideNavBar
    
    SKULSHADY: Cleanup the overlays
    
    Change-Id: Idfb4943b54a6c6c674e141eda484377ad5e1c9ad
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4796,6 +4796,12 @@ public final class Settings {
         private static final Validator DOUBLE_TAP_SLEEP_GESTURE_VALIDATOR =
                 BOOLEAN_VALIDATOR;
 
+        /** @hide */
+        public static final String GESTURE_PILL_TOGGLE = "gesture_pill_toggle";
+
+        /** @hide */
+        private static final Validator GESTURE_PILL_TOGGLE_VALIDATOR = BOOLEAN_VALIDATOR;
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -4867,6 +4873,7 @@ public final class Settings {
             ANBI_ENABLED_OPTION,
             QS_HEADER_BG_ALPHA,
             DOUBLE_TAP_SLEEP_GESTURE,
+            GESTURE_PILL_TOGGLE,
         };
 
         /**
@@ -4997,6 +5004,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_HEADER_BG_ALPHA);
             PRIVATE_SETTINGS.add(SCREENSHOT_DEFAULT_MODE);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
+            PRIVATE_SETTINGS.add(GESTURE_PILL_TOGGLE);
         }
 
         /**
@@ -5102,6 +5110,7 @@ public final class Settings {
             VALIDATORS.put(QS_HEADER_BG_ALPHA, QS_HEADER_BG_ALPHA_VALIDATOR);
             VALIDATORS.put(DOUBLE_TAP_SLEEP_GESTURE,
                     DOUBLE_TAP_SLEEP_GESTURE_VALIDATOR);
+            VALIDATORS.put(GESTURE_PILL_TOGGLE, GESTURE_PILL_TOGGLE_VALIDATOR);
         }
 
         /**
