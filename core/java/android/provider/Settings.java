267f6108fcd3bcee9aa684184a12bdd79c03c584
qiongz <qiongz@codeaurora.org>
2020-05-09 14:33:14 +0800

    FR30706: Add a playing tone setting.
    Change-Id: I5028a14088c30a8a5e6d89145c19bbbc69a0e9ef
    CRs-Fixed: 2663751

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3946,6 +3946,13 @@ public final class Settings {
         public static final String DISPLAY_COLOR_MODE = "display_color_mode";
 
         /**
+         * Whether to play tone while outgoing call is accepted.
+         * The value 1 - vibrate, 0 - not
+         * @hide
+         */
+        public static final String CALL_CONNECTED_TONE_ENABLED = "call_connected_tone_enabled";
+
+        /**
          * The user selected min refresh rate in frames per second.
          *
          * If this isn't set, 0 will be used.
@@ -5007,6 +5014,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
+            PRIVATE_SETTINGS.add(CALL_CONNECTED_TONE_ENABLED);
         }
 
         /**
