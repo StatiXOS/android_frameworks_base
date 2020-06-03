d309c73d42aa92d96cb232d04905816e59bc0ee5
Call190Now <call190now@statixos.com>
2020-06-03 18:35:14 -0400

    Revert "SystemUI: tuner: status bar battery fixes"
    This reverts commit c75a814be72e9bca14eabc97e0f18286c93ec1f0.
    
    Change-Id: I6b356de965d137de0471b3e3c458f41fb667a406

--- packages/SystemUI/src/com/android/systemui/tuner/ClockPreference.java
+++ packages/SystemUI/src/com/android/systemui/tuner/ClockPreference.java
@@ -11,48 +11,44 @@
  * KIND, either express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-
 package com.android.systemui.tuner;
 
-import static android.provider.Settings.System.SHOW_BATTERY_PERCENT;
-
 import android.content.Context;
-import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.AttributeSet;
 
 import androidx.preference.DropDownPreference;
 
-import com.android.internal.logging.MetricsLogger;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.Dependency;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
+import com.android.systemui.statusbar.policy.Clock;
 
-public class BatteryPreference extends DropDownPreference implements TunerService.Tunable {
+public class ClockPreference extends DropDownPreference implements TunerService.Tunable {
 
-    private static final String PERCENT = "percent";
+    private static final String SECONDS = "seconds";
     private static final String DEFAULT = "default";
     private static final String DISABLED = "disabled";
 
-    private final String mBattery;
-    private boolean mBatteryEnabled;
-    private boolean mHasPercentage;
+    private final String mClock;
+    private boolean mClockEnabled;
+    private boolean mHasSeconds;
     private ArraySet<String> mBlacklist;
     private boolean mHasSetValue;
+    private boolean mReceivedSeconds;
+    private boolean mReceivedClock;
 
-    public BatteryPreference(Context context, AttributeSet attrs) {
+    public ClockPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mBattery = context.getString(com.android.internal.R.string.status_bar_battery);
-        setEntryValues(new CharSequence[] {PERCENT, DEFAULT, DISABLED });
+        mClock = context.getString(com.android.internal.R.string.status_bar_clock);
+        setEntryValues(new CharSequence[] { SECONDS, DEFAULT, DISABLED });
     }
 
     @Override
     public void onAttached() {
         super.onAttached();
-        mHasPercentage = Settings.System.getInt(getContext().getContentResolver(),
-                SHOW_BATTERY_PERCENT, 0) != 0;
-        Dependency.get(TunerService.class).addTunable(this, StatusBarIconController.ICON_BLACKLIST);
+        Dependency.get(TunerService.class).addTunable(this, StatusBarIconController.ICON_BLACKLIST,
+                Clock.CLOCK_SECONDS);
     }
 
     @Override
@@ -64,17 +60,21 @@ public class BatteryPreference extends DropDownPreference implements TunerServic
     @Override
     public void onTuningChanged(String key, String newValue) {
         if (StatusBarIconController.ICON_BLACKLIST.equals(key)) {
+            mReceivedClock = true;
             mBlacklist = StatusBarIconController.getIconBlacklist(newValue);
-            mBatteryEnabled = !mBlacklist.contains(mBattery);
+            mClockEnabled = !mBlacklist.contains(mClock);
+        } else if (Clock.CLOCK_SECONDS.equals(key)) {
+            mReceivedSeconds = true;
+            mHasSeconds = newValue != null && Integer.parseInt(newValue) != 0;
         }
-        if (!mHasSetValue) {
+        if (!mHasSetValue && mReceivedClock && mReceivedSeconds) {
             // Because of the complicated tri-state it can end up looping and setting state back to
             // what the user didn't choose.  To avoid this, just set the state once and rely on the
             // preference to handle updates.
             mHasSetValue = true;
-            if (mBatteryEnabled && mHasPercentage) {
-                setValue(PERCENT);
-            } else if (mBatteryEnabled) {
+            if (mClockEnabled && mHasSeconds) {
+                setValue(SECONDS);
+            } else if (mClockEnabled) {
                 setValue(DEFAULT);
             } else {
                 setValue(DISABLED);
@@ -84,13 +84,12 @@ public class BatteryPreference extends DropDownPreference implements TunerServic
 
     @Override
     protected boolean persistString(String value) {
-        final boolean v = PERCENT.equals(value);
-        MetricsLogger.action(getContext(), MetricsEvent.TUNER_BATTERY_PERCENTAGE, v);
-        Settings.System.putInt(getContext().getContentResolver(), SHOW_BATTERY_PERCENT, v ? 1 : 0);
+        Dependency.get(TunerService.class).setValue(Clock.CLOCK_SECONDS, SECONDS.equals(value) ? 1
+                : 0);
         if (DISABLED.equals(value)) {
-            mBlacklist.add(mBattery);
+            mBlacklist.add(mClock);
         } else {
-            mBlacklist.remove(mBattery);
+            mBlacklist.remove(mClock);
         }
         Dependency.get(TunerService.class).setValue(StatusBarIconController.ICON_BLACKLIST,
                 TextUtils.join(",", mBlacklist));
