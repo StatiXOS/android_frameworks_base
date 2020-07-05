6e638c072457fba63b3766b99dbe97d62995e668
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-07-05 22:58:01 +0300

    FODCircleView: Post calls of main hal functions on background thread
    Change-Id: Ie3175fd143a83a87f985e3cb3a668b9d589c5903

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -45,6 +45,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardSecurityModel.SecurityMode;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
+import com.android.settingslib.utils.ThreadUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.tuner.TunerService;
@@ -396,7 +397,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         setKeepScreenOn(true);
 
         setDim(true);
-        dispatchPress();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchPress();
+        });
 
         setImageDrawable(null);
         updatePosition();
@@ -410,7 +413,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         setImageResource(R.drawable.fod_icon_default);
         invalidate();
 
-        dispatchRelease();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchRelease();
+        });
         setDim(false);
 
         setKeepScreenOn(false);
@@ -430,7 +435,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         updatePosition();
 
         Dependency.get(TunerService.class).addTunable(this, SCREEN_BRIGHTNESS);
-        dispatchShow();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchShow();
+        });
         setVisibility(View.VISIBLE);
     }
 
@@ -438,7 +445,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         setVisibility(View.GONE);
         Dependency.get(TunerService.class).removeTunable(this);
         hideCircle();
-        dispatchHide();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchHide();
+        });
     }
 
     private void updateAlpha() {
