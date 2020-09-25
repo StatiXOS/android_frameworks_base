fac4917d9837f0d67fc99c429f50353bcdc899a8
Danny Lin <danny@kdrag0n.dev>
2020-09-25 14:30:21 -0700

    SystemUI: Remove QS navbar scrim
    In the age of flat material design, this scrim looks outdated and
    out-of-place. It's also quite ugly in the QS edit screen, where it
    overlaps the edit view for no apparent reason because the edit view is
    already fully opaque.
    
    The existing notification shade scrim provides enough contrast with the
    contents of the app behind the QS, and background blurring helps even
    more with that, so let's just remove this scrim altogether.
    
    Change-Id: Iaba142751aa033b30a44e23888db7e7664f0ef9f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -266,7 +266,6 @@ public class NotificationPanelViewController extends PanelViewController {
     private QS mQs;
     private FrameLayout mQsFrame;
     private KeyguardStatusView mKeyguardStatusView;
-    private View mQsNavbarScrim;
     private NotificationsQuickSettingsContainer mNotificationContainerParent;
     private NotificationStackScrollLayout mNotificationStackScroller;
     private boolean mAnimateNextPositionUpdate;
@@ -621,7 +620,6 @@ public class NotificationPanelViewController extends PanelViewController {
         mNotificationStackScroller.setOnEmptySpaceClickListener(mOnEmptySpaceClickListener);
         addTrackingHeadsUpListener(mNotificationStackScroller::setTrackingHeadsUp);
         mKeyguardBottomArea = mView.findViewById(R.id.keyguard_bottom_area);
-        mQsNavbarScrim = mView.findViewById(R.id.qs_navbar_scrim);
         mLastOrientation = mResources.getConfiguration().orientation;
 
         initBottomArea();
@@ -1699,9 +1697,6 @@ public class NotificationPanelViewController extends PanelViewController {
                         || mQsExpansionFromOverscroll));
         updateEmptyShadeView();
 
-        mQsNavbarScrim.setVisibility(
-                mBarState == StatusBarState.SHADE && mQsExpanded && !mStackScrollerOverscrolling
-                        && mQsScrimEnabled ? View.VISIBLE : View.INVISIBLE);
         if (mKeyguardUserSwitcher != null && mQsExpanded && !mStackScrollerOverscrolling) {
             mKeyguardUserSwitcher.hideIfNotSimple(true /* animate */);
         }
@@ -1725,10 +1720,6 @@ public class NotificationPanelViewController extends PanelViewController {
         if (mBarState == StatusBarState.SHADE_LOCKED || mBarState == StatusBarState.KEYGUARD) {
             updateKeyguardBottomAreaAlpha();
             updateBigClockAlpha();
-        }
-        if (mBarState == StatusBarState.SHADE && mQsExpanded && !mStackScrollerOverscrolling
-                && mQsScrimEnabled) {
-            mQsNavbarScrim.setAlpha(getQsExpansionFraction());
         }
 
         if (mAccessibilityManager.isEnabled()) {
