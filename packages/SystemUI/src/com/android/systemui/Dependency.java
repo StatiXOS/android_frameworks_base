641d7229b7071614c285aea04d83663eceaefeb2
maxwen <max.weninger@gmail.com>
2020-09-28 15:40:30 +0200

    SystemUI: add StatixSettingsService
    statix-11 edits:
      - Rebrand.
    
    arrow-11.0 edits:
      - Rebrand the SettingsService Observer.
    
      TODO:
        - Convert all our current individual observers to make use of this.
    
    Change-Id: I7909aab11b352fb058bf122102d85b095418fbb9
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -48,6 +48,7 @@ import com.android.systemui.keyguard.ScreenLifecycle;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.media.dialog.MediaOutputDialogFactory;
 import com.android.systemui.model.SysUiState;
+import com.android.systemui.statix.StatixSettingsService;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.PluginDependencyProvider;
@@ -328,6 +329,7 @@ public class Dependency {
     @Inject Lazy<ProtoTracer> mProtoTracer;
     @Inject Lazy<Divider> mDivider;
     @Inject Lazy<MediaOutputDialogFactory> mMediaOutputDialogFactory;
+    @Inject Lazy<StatixSettingsService> mStatixSettingsService;
 
     @Inject
     public Dependency() {
@@ -525,6 +527,7 @@ public class Dependency {
 
         mProviders.put(RecordingController.class, mRecordingController::get);
         mProviders.put(Divider.class, mDivider::get);
+        mProviders.put(StatixSettingsService.class, mStatixSettingsService::get);
 
         mProviders.put(MediaOutputDialogFactory.class, mMediaOutputDialogFactory::get);
 
--- packages/SystemUI/src/com/android/systemui/dagger/DependencyBinder.java
+++ packages/SystemUI/src/com/android/systemui/dagger/DependencyBinder.java
@@ -29,6 +29,8 @@ import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.GlobalActions;
 import com.android.systemui.plugins.VolumeDialogController;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.statix.StatixSettingsService;
+import com.android.systemui.statix.StatixSettingsServiceImpl;
 import com.android.systemui.power.PowerNotificationWarnings;
 import com.android.systemui.power.PowerUI;
 import com.android.systemui.qs.QSHost;
@@ -265,4 +267,10 @@ public abstract class DependencyBinder {
     @Binds
     public abstract RingerModeTracker provideRingerModeTracker(
             RingerModeTrackerImpl ringerModeTrackerImpl);
+
+    /**
+     */
+    @Binds
+    public abstract StatixSettingsService provideStatixSettingsService(
+            StatixSettingsServiceImpl controllerImpl);
 }
