e132c4f16d086e88856465eb2d00ddc3d4bf6cbb
Vachounet <vachounet@live.fr>
2020-10-14 11:43:10 +0200

    SystemUI: remove black bg from power menu
    And allow blur
    
    stx: set lower transparency
    
    Change-Id: I87092d57ea457e6d6783c87b8a64b4e69dec2d8a

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -2548,7 +2548,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             initializeWalletView();
             if (mBackgroundDrawable == null) {
                 mBackgroundDrawable = new ScrimDrawable();
-                mScrimAlpha = 1.0f;
+                mScrimAlpha = 0.90f;
             }
             getWindow().setBackgroundDrawable(mBackgroundDrawable);
         }
@@ -2585,7 +2585,6 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             if (!(mBackgroundDrawable instanceof ScrimDrawable)) {
                 return;
             }
-            ((ScrimDrawable) mBackgroundDrawable).setColor(Color.BLACK, animate);
             View decorView = getWindow().getDecorView();
             if (colors.supportsDarkText()) {
                 decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR |
