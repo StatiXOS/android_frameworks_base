f23e6dde14519fbbb277a41881714a19fb6b568e
Filippo <zzkw35@gmail.com>
2020-11-12 05:43:17 -0800

    base: Don't allow to double tap to sleep on LS when the device is on Ambient
    Fixup: https://bit.ly/2ItubzW
    
    All the credits go to Ezio: https://bit.ly/3eQIlXu
    
    Change-Id: I98f6a6356f64dba03d00ae844bd0f83df5b9013c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -3196,7 +3196,7 @@ public class NotificationPanelViewController extends PanelViewController {
                     mDoubleTapGesture.onTouchEvent(event);
                 }
 
-                if (mIsLockscreenDoubleTapEnabled
+                if (mIsLockscreenDoubleTapEnabled && !mPulsing && !mDozing
                         && mBarState == StatusBarState.KEYGUARD) {
                     mLockscreenDoubleTapToSleep.onTouchEvent(event);
                 }
