8bf034dac6c2df8d1ce9ec9baea6bebf04fb5274
ali <abittin@gmail.com>
2020-12-07 10:09:35 +0000

    base: Ambient Display battery toggle [1/2]
    Change-Id: I81e18e037549daa7ffb6a659dcc8550307eb5080

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4986,6 +4986,12 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Whether to show battery percent on ambient bottom
+         * @hide
+         */
+        public static final String AMBIENT_BATTERY_PERCENT = "ambient_battery_percent";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
