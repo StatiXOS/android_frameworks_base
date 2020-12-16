0694825be82a8fd12132ad80d1b890aa4758ba42
Danny Lin <danny@kdrag0n.dev>
2020-12-15 16:58:40 -0800

    SystemUI: Skip screen-on animation in all scenarios
    Unlike turning the screen off, turning it on is a time-critical
    operation that should be done as fast as possible without a slow
    animation. It's not only noticeable when using capacitive fingerprint
    sensors.
    
    Change-Id: I87276128456afd9f535742477d9f76d025a12539

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4180,8 +4180,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         boolean launchingAffordanceWithPreview =
                 mNotificationPanelViewController.isLaunchingAffordanceWithPreview();
-        mScrimController.setLaunchingAffordanceWithPreview(launchingAffordanceWithPreview
-                || mBiometricUnlockController.isWakeAndUnlock());
+        mScrimController.setLaunchingAffordanceWithPreview(true);
 
         if (mBouncerShowing) {
             // Bouncer needs the front scrim when it's on top of an activity,
