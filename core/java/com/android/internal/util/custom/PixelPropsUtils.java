468da2b9ca691c3fea695302b97f57ec7c871080
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-11-06 10:09:09 -0400

    PixelPropUtils: Spoof to Pixel 5 (redfin)
    * Last QCOM device from Google. Prevents breakage of various features
      that expect special hardware in Google Tensor.
    
    Change-Id: I80fee436f49bd88bdfc0f21cc345f0bc8bc889cc

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -48,7 +48,7 @@ public class PixelPropsUtils {
         propsToChange.put("DEVICE", "redfin");
         propsToChange.put("PRODUCT", "redfin");
         propsToChange.put("MODEL", "Pixel 5");
-        propsToChange.put("FINGERPRINT", "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys");
+        propsToChange.put("FINGERPRINT", "google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys");
     }
 
     public static void setProps(String packageName) {
