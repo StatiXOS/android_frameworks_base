2e6e77cda6c12f317b8a5b286ed9b55880bf5263
Vaisakh Murali <mvaisakh@statixos.com>
2022-05-03 07:00:39 +0000

    PixelPropUtils: Update redfin fingerprint to May
    Change-Id: I18e4327f728f62ee4cf8ce5e400f266bb440a87e
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -68,7 +68,7 @@ public class PixelPropsUtils {
         propsToChange.put("DEVICE", "redfin");
         propsToChange.put("PRODUCT", "redfin");
         propsToChange.put("MODEL", "Pixel 5");
-        propsToChange.put("FINGERPRINT", "google/redfin/redfin:12/SP2A.220405.003/8210211:user/release-keys");
+        propsToChange.put("FINGERPRINT", "google/redfin/redfin:12/SP2A.220505.002/8353555:user/release-keys");
     }
 
     public static void setProps(Application app) {
