5eeca8e4763ebe34f1e4da2a4663266d0c5039d1
Sourajit Karmakar <sourajitk@statixos.com>
2023-01-30 10:54:51 -0500

    figure this out
    Change-Id: Ib4420c50ee8af356b8fd5e5bfb94b9dfc2e3aa40

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -45,19 +45,14 @@ public class PropImitationHooks {
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PROCESS_GMS_UNSTABLE = PACKAGE_GMS + ".unstable";
 
-    private static final String PACKAGE_VELVET = "com.google.android.googlequicksearchbox";
-    private static final String PACKAGE_TURBO = "com.google.android.apps.turbo";
-    private static final String PACKAGE_GBOARD = "com.google.android.inputmethod.latin";
-    private static final String PACKAGE_SUBSCRIPTION_RED = "com.google.android.apps.subscriptions.red";
-    private static final String PACKAGE_WILDLIFE = "com.google.android.apps.privacy.wildlife";
-    private static final Map<String, Object> sP7Props = new HashMap<>();
+    private static final Map<String, Object> sP5Props = new HashMap<>();
     static {
-        sP7Props.put("BRAND", "google");
-        sP7Props.put("MANUFACTURER", "Google");
-        sP7Props.put("DEVICE", "cheetah");
-        sP7Props.put("PRODUCT", "cheetah");
-        sP7Props.put("MODEL", "Pixel 7 Pro");
-        sP7Props.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ1A.230105.002/9325679:user/release-keys");
+        sP5Props.put("BRAND", "google");
+        sP5Props.put("MANUFACTURER", "Google");
+        sP5Props.put("DEVICE", "redfin");
+        sP5Props.put("PRODUCT", "redfin");
+        sP5Props.put("MODEL", "Pixel 5");
+        sP5Props.put("FINGERPRINT", "google/redfin/redfin:13/TQ1A.230105.001/9292298:user/release-keys");
     }
 
     private static volatile boolean sIsGms = false;
@@ -80,10 +75,9 @@ public class PropImitationHooks {
         } else if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
             dlog("Setting stock fingerprint for: " + packageName);
             setPropValue("FINGERPRINT", sStockFp);
-        } else if (packageName.equals(PACKAGE_VELVET) || packageName.equals(PACKAGE_TURBO) || packageName.equals(PACKAGE_GBOARD)
-                   || packageName.equals(PACKAGE_SUBSCRIPTION_RED) || packageName.equals(PACKAGE_WILDLIFE)) {
-            dlog("Spoofing Pixel 7 Pro for: " + packageName);
-            sP7Props.forEach((k, v) -> setPropValue(k, v));
+        } else if (packageName.startsWith("com.google.")) {
+            dlog("Spoofing Pixel 5 for: " + packageName);
+            sP5Props.forEach((k, v) -> setPropValue(k, v));
         }
     }
 
