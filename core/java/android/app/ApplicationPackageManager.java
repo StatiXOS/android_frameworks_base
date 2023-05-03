b8e54f5733a41373cea081c008f4488895644956
John Galt <johngaltfirstrun@gmail.com>
2023-05-03 13:39:06 +0800

    core: block 2022+ pixel experience xmls to ASI
    Resolves downloads on play store updated ASI (newer than
    image-cheetah-tq2a.230405.003.e1 shipping).
    
    Change-Id: I3c4adbe0a2b4d7b08824b6ff508273a02c4ff127

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -816,6 +816,12 @@ public class ApplicationPackageManager extends PackageManager {
 		name.contains("PIXEL_2022_EXPERIENCE") ||
 		name.contains("PIXEL_2022_MIDYEAR_EXPERIENCE"))) {
 	    return false;
+	} else if (packageName != null &&
+		packageName.contains("com.google.android.as") &&
+		(name.contains("PIXEL_2022_EXPERIENCE") ||
+		name.contains("PIXEL_2022_MIDYEAR_EXPERIENCE"))) {
+	    return false;
+
 	}
         return mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
     }
