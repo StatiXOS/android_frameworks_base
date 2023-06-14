9f27bdcaea6e8f9a70e811da130d70e47cb4a3af
Anay Wadhera <anay1018@gmail.com>
2023-06-14 01:41:14 -0400

    SystemUI: Move FlashlightTile binding to its own module
    Change-Id: Ifad882554c67ea1e07c50967a52d9b7ae163368c

--- packages/SystemUI/src/com/android/systemui/dagger/ReferenceSystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/ReferenceSystemUIModule.java
@@ -66,6 +66,7 @@ import com.android.systemui.statusbar.policy.AospPolicyModule;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedControllerImpl;
+import com.android.systemui.statusbar.policy.FlashlightModule;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
 import com.android.systemui.statusbar.policy.HeadsUpManagerLogger;
 import com.android.systemui.statusbar.policy.IndividualSensorPrivacyController;
@@ -99,6 +100,7 @@ import javax.inject.Named;
         AospPolicyModule.class,
         BatterySaverModule.class,
         BluetoothModule.class,
+        FlashlightModule.class,
         GestureModule.class,
         MediaModule.class,
         MultiUserUtilsModule.class,
