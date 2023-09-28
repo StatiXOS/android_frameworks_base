96f514417d621ea5b63fa862a655cbd39d60dda8
sean griffiths <yehonatan@mail.ru>
2023-09-28 22:48:06 +0100

    Revert "PowerMenu: Hide emergency affordance if not selected"
    This reverts commit 14d78ecffb80d2aef3a54ff190bec12d9e4fd106.

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -142,7 +142,6 @@ import com.android.systemui.util.settings.GlobalSettings;
 import com.android.systemui.util.settings.SecureSettings;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Executor;
@@ -672,10 +671,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         CurrentUserProvider currentUser = new CurrentUserProvider();
 
         // make sure emergency affordance action is first, if needed
-        boolean showEmergencyAffordance = Arrays.stream(mActions)
-                .anyMatch(GLOBAL_ACTION_KEY_EMERGENCY::equals);
-        if (showEmergencyAffordance &&
-                mEmergencyAffordanceManager.needsEmergencyAffordance()) {
+        if (mEmergencyAffordanceManager.needsEmergencyAffordance()) {
             addIfShouldShowAction(tempActions, new EmergencyAffordanceAction());
             addedKeys.add(GLOBAL_ACTION_KEY_EMERGENCY);
         }
