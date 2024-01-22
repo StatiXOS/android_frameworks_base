91b38c2ee7e1d2692aa58e6b09e869ef588b7660
ralph950412 <ralph950412@gmail.com>
2024-01-22 09:53:57 +0800

    PropImitationHooks: Spoof Motorola "clark"
    Change-Id: I9626014ed660031468f5c63b0e22a6e14dacdf68

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -104,12 +104,13 @@ public class PropImitationHooks {
 
     private static void spoofBuildGms() {
         // Alter model name and fingerprint to avoid hardware attestation enforcement
-        setPropValue("FINGERPRINT", "essential/mata/mata:8.1.0/OPM1.180104.092/224:user/release-keys");
-        setPropValue("PRODUCT", "mata");
-        setPropValue("DEVICE", "mata");
-        setPropValue("MANUFACTURER", "Essential Products");
-        setPropValue("BRAND", "essential");
-        setPropValue("MODEL", "PH-1");
+        setPropValue("MANUFACTURER", "Google");
+        setPropValue("MODEL", "Pixel");
+        setPropValue("FINGERPRINT", "google/sailfish/sailfish:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+        setPropValue("BRAND", "google");
+        setPropValue("PRODUCT", "sailfish");
+        setPropValue("DEVICE", "sailfish");
+
     }
 
     private static boolean isCallerSafetyNet() {
