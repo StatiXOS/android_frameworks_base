a3a4d8090376a2f85b7fb7595fd4ee00524b1ce4
Pranaya Deomani <pranayadmn@proton.me>
2024-05-03 12:04:17 +0530

    BrightnessController: Add null check before haptic feedback
    Fixes NPE on devices which are incapable of haptic feedback.
    
    Change-Id: I67538559978e8b0dc2282d06b09675ffea6ba3a6
    Signed-off-by: Pranaya Deomani <pranayadmn@proton.me>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -435,7 +435,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         }
 
         // Give haptic feedback only if brightness is changed manually
-        if (tracking)
+        if (mVibrator != null && tracking)
             mVibrator.vibrate(BRIGHTNESS_SLIDER_HAPTIC);
 
         if (!tracking) {
