3dab08b452b782931bcc5df0a6c907cc4203faf2
Anay Wadhera <anay1018@gmail.com>
2024-05-10 18:38:12 +0000

    SystemUI: Introduce Flashlight Strength Tile
    Squashed:
    * FlashlightStrengthTile: Ignore sliding when tile is inactive
    * FlashlightStrengthTile: Use correct default value for percentage drawable
    * FlashlightStrengthTile: Hide percentage when tile is inactive
    * FlashlightStrengthTile: Don't turn off for percentage > 0
    * FlashlightStrengthTile: Refresh state on torch strength level changes
    * SliderQSTile: Use different bg color for =>90%
    * QSTileViewImpl: Allow to override getBackgroundColorForState()
    * QsTileViewImpl: Make setColor() as protected
    * Update to Android 15
    
    Co-authored-by: Jake Weinstein <jake@aospa.co>
    Co-authored-by: ralph950412 <ralph950412@gmail.com>
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Change-Id: Ic9b423f1701b0047c148e90a772db84f8dea0b35
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -159,7 +159,7 @@ constructor(
     protected lateinit var qsTileBackground: RippleDrawable
     private lateinit var qsTileFocusBackground: Drawable
     private lateinit var backgroundDrawable: LayerDrawable
-    private lateinit var backgroundBaseDrawable: Drawable
+    protected lateinit var backgroundBaseDrawable: Drawable
     private lateinit var backgroundOverlayDrawable: Drawable
 
     private var backgroundColor: Int = 0
@@ -801,7 +801,7 @@ constructor(
         setOverlayColor(overlayColor)
     }
 
-    private fun setColor(color: Int) {
+    protected fun setColor(color: Int) {
         backgroundBaseDrawable.mutate().setTint(color)
         backgroundColor = color
     }
@@ -858,7 +858,7 @@ constructor(
         return locInScreen.get(1) >= -height
     }
 
-    private fun getBackgroundColorForState(state: Int, disabledByPolicy: Boolean = false): Int {
+    protected open fun getBackgroundColorForState(state: Int, disabledByPolicy: Boolean = false): Int {
         return when {
             state == Tile.STATE_UNAVAILABLE || disabledByPolicy -> colorUnavailable
             state == Tile.STATE_ACTIVE -> colorActive
